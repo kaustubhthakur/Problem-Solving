3175b21588a6f2048c7891ee57985640a5de0475
Kaustubh <kaustubhthakur76@gmail.com>
2023-02-27 00:14:14 +0530

    pupil commit

---
+++ Pupil/others/1.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+int n,m;
+cin>>n>>m;
+vector<int>a(n);
+for(int i=0;i<n;i++) cin>>a[i];
+
+vector<int>b(m);
+for(int i=0;i<m;i++) cin>>b[i];
+int ans =0;
+
+
+for(int i=0;i<m;i++)
+{
+    cout<<ans<<endl;
+}
+}
+int main()
+{
+
+
+    solve();
+
+
+
+    return 0;
+}
