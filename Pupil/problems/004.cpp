475f3a1d3e691d1c001060b29fc22dcfb8b3d90d
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-01 20:43:37 +0530

    pupil commit

--- Pupil/problems/004.cpp
+++ Pupil/problems/004.cpp
@@ -11,6 +11,26 @@ while(t--)
     cin>>l>>r>>x;
     int a,b;
     cin>>a>>b;
+    if(a==b)
+    {
+        cout<<0<<endl;
+    }
+    else if(abs(a-b)>=x)
+    {
+        cout<<1<<endl;
+    }
+    else if(r-max(a,b)>=x || min(a,b)-l>=x)
+    {
+        cout<<2<<endl;
+    }
+    else if(r-b>=x && a-l>=x || r-a>=x && b-l>=x)
+    {
+        cout<<3<<endl;
+    }
+    else 
+    {
+        cout<<-1<<endl;
+    }
     
   
 
