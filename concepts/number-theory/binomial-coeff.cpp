54ce02a282448d1d97e66a64dfc7dc81dd9e8788
Kaustubh <kaustubhthakur76@gmail.com>
2023-03-08 00:15:04 +0530

    initlal commit

---
+++ concepts/number-theory/binomial-coeff.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int f[10000001];
+const int p = 1e8;
+int c (int n,int k)
+{
+    if(k>n)
+    {
+        return 0;
+    }
+    else 
+    {
+        return 
+    }
+}
+int main()
+{
+    f[0]=f[1]=1;
+    for(int i=1;i<=1000000;++i)
+    {
+      f[i] = (f[i]*1LL*i)%p; 
+    }
+    int q;
+    int n,k;
+    while(q--)
+    {
+        cin>>n>>k;
+        cout<<
+    }
+
+    return 0;
+}
